74a1bbd63288259e5a3547354d40d602106f5cb6
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2026-03-04 15:45:35 +0300

    Add files via upload

---
+++ 04.03.py
@@ -0,0 +1,124 @@
+# from dataclasses import dataclass
+# @dataclass
+# class Talaba:
+#     ism: str
+#     yosh: int
+#     kurs: int
+#
+# Maqasad = Talaba("Ali", 19, 2)
+# print(Talaba)
+
+# # 3
+# from dataclasses import dataclass
+# @dataclass
+# class Telefon:
+#     brand: str
+#     model: int
+#     narx: int
+#
+# Dokon = Telefon("Iphone", 17, 1700)
+# print(Dokon)
+
+# # 2
+# from dataclasses import dataclass
+# @dataclass
+# class Kitob:
+#     nomi: str
+#     muallif: str
+#     sahifa_soni: int
+#
+# Dokon1 = Kitob("Maqasa haqida hikoya","Maqsadbek",41)
+# print(Dokon1)
+
+# # 4
+# from dataclasses import dataclass
+# @dataclass
+# class Avtomobil:
+#     marka:str
+#     rang:str
+#     yil:int
+#
+# Bozor = Avtomobil("Nexi","Qizil",2020)
+# print(Bozor)
+
+# # 5
+# from dataclasses import dataclass
+# @dataclass
+# class Shahar:
+#     nomi:str
+#     davlat:str
+#     aholi:str
+#
+# dav = Shahar("London","Angliya","72mln")
+# print(dav)
+
+##6-masala
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Kompyuter:
+#     protssesor: str
+#     ram: str
+#     xotira: int
+# Kompyuter = Kompyuter("CORE i7", "32", 512 )
+# print(Kompyuter)
+
+
+##7-masala
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Film:
+#     nomi: str
+#     janr: str
+#     yil: int
+# Film =Film("Garri Poter" ,"qo'rqinchli", 2011)
+# print(Film)
+
+
+##8-masala
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Hisob:
+#     ism: str
+#     hisob_raqam: int
+#     balans: int
+# Hisob = Hisob("Xusniddin", 9860345673452345, 20_000_000)
+# print(Hisob)
+
+
+##9-masala
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Oqituvchi:
+#     ism: str
+#     fan: str
+#     tajriba_yili: int
+# Oqituvchi = Oqituvchi("Sapargul", "Biologiya", "34")
+# print(Oqituvchi)
+
+
+##10-masala
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Mahsulot:
+#     nomi: str
+#     narx:int
+#     miqdor: int
+# Mahsulot = Mahsulot("Tuxum", 45_000, 2)
+# print(Mahsulot)
+
+
+
+
+
+
+
+
+
+
+
+
